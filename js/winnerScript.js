d1033f36a012afb613fe787aa3bb72e4a1d77e7a
Jehru <jehru.e.harris@gmail.com>
2021-05-07 18:10:39 +1000

    Adding Comments

--- js/winnerScript.js
+++ js/winnerScript.js
@@ -1,14 +1,16 @@
+// When document dom is ready
 $(document).ready(function() {
 
     // Checks if the session storage API works
     if(typeof(Storage)!=="undefined"){
 
-        // Save the previous session values as a variable
+        // Retrieves the previous moves, minute and second variables using sessions storage
+        // Store them as variables to append to the page
         var lastMoves = sessionStorage.getItem("lastmoves");
         var lastMin = sessionStorage.getItem("lastmin");
         var lastSec = sessionStorage.getItem("lastsec");
 
-        // Append the move and time counts to the page
+        // Appends the move and time counts to the page
         $('.moveCount').append('<p> Your Last Moves: ' + lastMoves + '<p>')
         $('.timeCount').append('<p> Your Last Time: ' + lastMin + ':' + lastSec + '<p>')
 
